09c97dfb52175897365e81508e84a9ec81dc59df
amjayy <33906054+amjayy@users.noreply.github.com>
2018-03-23 16:21:42 -0500

    Add files via upload

---
+++ New Js ch.3/javascript/multiple-objects.js
@@ -0,0 +1,21 @@
+function Hotel(name, rooms, booked) {
+	this.name = name;
+	this.rooms = rooms;
+	this.booked = booked;
+	this.checkAvailability = function (){
+		return this.rooms - this booked;
+	};
+}
+
+var jxnHotel = new Hotel ('Jxn', 40, 25);
+var kamdenHotel = new Hotel ('Kamden', 120, 77);
+
+var details1 = jxnHotel.name + 'rooms: ';
+	details1 += jxnHotel.checkAvailability();
+var elHotel1 = document.getElementById('hotel1');
+elHotel1.textContent = details1;
+
+var details2 = kamdenHotel.name + 'rooms': ';
+	details2 += kamdenHotel.checkAvailability();
+var elHotel2 = document.getElementById('hotel');
+elHotel2.textContent = details2
